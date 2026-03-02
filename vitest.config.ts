593e40f5b053064f5c111ac8fd9db0858777623c
Matisse <MatissesProjects@users.noreply.github.com>
2026-03-02 11:46:31 -0800

    chore: rename __tests__ directory to tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   test: {
     environment: 'happy-dom',
     globals: true,
-    setupFiles: ['./src/__tests__/setup.ts'],
+    setupFiles: ['./src/tests/setup.ts'],
   },
 });
